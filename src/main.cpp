4710747cdd7b0e8b76cc50079aaddad2ca423ee0
otreblan <otreblain@gmail.com>
2019-11-14 19:28:18 -0500

    Por ahora hay solo una pantalla negra

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,26 @@
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world\n";
-
 	sf::RenderWindow window(sf::VideoMode(), "Clones", sf::Style::Fullscreen);
+
+	while(window.isOpen())
+	{
+		sf::Event event;
+		while(window.pollEvent(event))
+		{
+			switch(event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+				default:
+					break;
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 
 	return 0;
 }
