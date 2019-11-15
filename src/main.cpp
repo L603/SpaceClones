eb481105cc5658cb10cf688ca9188c769eab5187
otreblan <otreblain@gmail.com>
2019-11-14 19:22:25 -0500

    Ahora si linkea SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char *argv[])
 {
 	std::cout << "Hello world\n";
+
+	sf::RenderWindow window(sf::VideoMode(), "Clones", sf::Style::Fullscreen);
+
 	return 0;
 }
