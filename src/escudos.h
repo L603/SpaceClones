6067b4e51ad7a55e03ecdc5f3c88b3a86690e3cf
gian <gian.machicado@utec.edu.pe>
2019-11-21 18:53:50 -0500

    nuevos escudos

--- src/escudos.h
+++ src/escudos.h
@@ -5,8 +5,31 @@
 #ifndef CLONE_ESCUDOS_H
 #define CLONE_ESCUDOS_H
 
+#include "damageable.h"
+#include "gameObject.h"
+#include <SFML/Graphics.hpp>
 
-class escudos {
+class Escudos: public GameObject, public sf::Drawable{
+private:
+    hp life;
+
+public:
+    explicit Escudos(Scene& _myScene);
+
+    virtual ~Escudos();
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states)const;
+
+    virtual void update();
+
+    static std::weak_ptr<Escudos> spawn(Scene& _myScene, sf::Vector2f position);
+
+
+
+
+
+
+
 
 };
 
