314be55515cd8863552869da332d1b9052abedbf
otreblan <otreblain@gmail.com>
2019-11-24 18:40:03 -0500

    Aún más macros

--- config.h.cmake
+++ config.h.cmake
@@ -2,7 +2,9 @@
 #define CONFIG_H
 
 #define DATA_DIR "${DATA_DIR}"
-
 #define BUILD_DATA_DIR "${BUILD_DATA_DIR}"
+
+#define BIN_DIR "${BIN_DIR}"
+#define BUILD_BIN_DIR "${BUILD_BIN_DIR}"
 
 #endif /*CONFIG_H*/
