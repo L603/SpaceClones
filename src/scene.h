92260f26fb3b07e3985ea15d4da2f7f28329ba01
gian <gian.machicado@utec.edu.pe>
2019-11-28 07:09:34 -0500

    puse mas comentarios

--- src/scene.h
+++ src/scene.h
@@ -24,6 +24,8 @@ private:
 	sf::RenderWindow window;
 
 	/// Aquí es donde se guardan todos los objetos del juego.
+	/// Tiene un puntero compartido, los punteros debiles se relacionan con este.
+	/// Se usan punteros inteligentes cuando dependen de otro puntero, como si fuese un ciclo.
 	std::vector<std::shared_ptr<GameObject>> objects;
 
 	/// Aquí esta todo lo que hay que renderizar.
@@ -33,7 +35,7 @@ private:
 	std::unordered_map<RigidBody::faction,
 		std::vector<std::weak_ptr<RigidBody>>> rigidBodies;
 
-	/// Una matriz para activar o desactivar colisiones.
+	/// Una matriz para activar o desactivar colisiones. Mapa, el primero el primero y segundo es un tipo de dato, nombre del mapa.
 	std::unordered_map<RigidBody::faction,
 		std::unordered_map<RigidBody::faction, bool>> collisionMatrix;
 
@@ -58,7 +60,7 @@ private:
 	/// Tiempo desde el último tick
 	timeT deltaTime;
 
-	/// Esto es para que GameObject pueda obtener el deltaTime
+	/// Esto es para que GameObject pueda obtener el deltaTime(El tiempo entre cada frame de un objeto)
 	friend timeT GameObject::deltaTime();
 	friend Nave::Nave(Scene& _myScene);
 
